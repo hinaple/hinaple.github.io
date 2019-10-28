b4c0cd8eafdc7fc608f4cf9aa2cbbbaefc65f228
root <root@DESKTOP-HQULBBK.localdomain>
2019-10-28 22:43:09 +0900

    기기별 최적화

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const height = window.innerHeight;
+
+
 swal({
     icon: "warning",
     title: "WAIT!",
@@ -19,23 +22,39 @@ swal({
 
 $(window).scroll(() => {
     var scroll = $(this).scrollTop();
-    console.log(scroll);
-    if(scroll > 1100) {
+    if(scroll >= height) {
         $(".tab").text("Bongdam ms meal");
         $(".url").val("http://meal.odcb.kr/");
+        $(".img.mealimg").css("animation-play-state", "running");
+        $(".content.mealc").css("animation-play-state", "running");
     }
-    else $(".tab").text("portfolio");
-    if(scroll > 2330) $(".tab").text("Bongdam ms calendar");
-    if(scroll > 3440) $(".tab").text("online chatting");
+    else {
+        $(".tab").text("portfolio");
+        $(".url").val("http://odcb.kr/");
+    }
+    if(scroll >= height * 2) {
+        $(".tab").text("Bongdam ms calendar");
+        $(".url").val("http://haksa.odcb.kr/");
+        $(".img.haksaimg").css("animation-play-state", "running");
+        $(".content.haksac").css("animation-play-state", "running");
+        $(".bibe").css("animation-play-state", "running");
+    }
+    if(scroll >= height * 3) {
+        $(".tab").text("online chatting");
+        $(".url").val("http://onchat.odcb.kr/");
+    }
 });
 
 var bCnt = 0;
 var blink;
+var blinking = true;
 $(".works").mouseover(() => {
-    if(bCnt % 2 == 1) $(".works").css("animation", "blink 1s " + (bCnt + 2) + " alternate-reverse ease-in-out");
+    if(!blinking) return;
+    if(bCnt <= 0) $(".works").css("animation", "worksSpread 1s");
+    else if(bCnt % 2 == 1) $(".works").css("animation", "blink 1s " + (bCnt + 2) + " alternate-reverse ease-in-out");
     else $(".works").css("animation", "blink 1s " + (bCnt + 1) + " alternate-reverse ease-in-out");
-    console.log(bCnt);
     clearInterval(blink);
+    blinking = false;
 });
 
 function toggleFullScreen() {
